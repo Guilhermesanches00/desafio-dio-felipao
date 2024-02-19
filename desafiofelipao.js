6856685bf8c020382736ca711c4aa69b88057f7a
Guilhermesanches00 <guilhermesanches2050@gmail.com>
2024-02-19 15:11:34 -0300

    Desafio concluido

---
+++ desafiofelipao.js
@@ -0,0 +1,31 @@
+let nomeDoHeroi = "Arqueiro"
+let Xp = 4800
+
+if(Xp <= 1000){
+   console.log(" O nivel de habilidade do " + nomeDoHeroi + " é ferro")
+    
+ }else if(Xp <= 2000)
+   console.log(" O nivel de habilidade do " + nomeDoHeroi + " é Bronze")
+    
+  else if(Xp <= 5000)
+  console.log(" O nivel de habilidade do " + nomeDoHeroi + " é Prata")
+  
+  else if(Xp <= 6000)
+  console.log(" O nivel de habilidade do " + nomeDoHeroi + " é Ouro")
+  
+  else if(Xp <= 7000)
+  console.log(" O nivel de habilidade do " + nomeDoHeroi + " é Platina")
+  
+  else if(Xp <= 8000)
+  console.log(" O nivel de habilidade do " + nomeDoHeroi + " é Ascendente")
+  
+  else if(Xp <= 9000)
+  console.log(" O nivel de habilidade do " + nomeDoHeroi + " é Imortal")
+  
+  else if(Xp <= 10000)
+  console.log(" O nivel de habilidade do " + nomeDoHeroi + " é Radiante")
+  
+  else if(Xp > 10000)
+  console.log(" O nivel de habilidade do " + nomeDoHeroi + " é Radiante")
+  
+// Desafio resolvido
